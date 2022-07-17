faf451b7ff9621bcb7b11fa13a2cb8f7ae5d6b51
Shady Hanna <shady22@my.yorku.ca>
2022-07-17 19:02:21 -0400

    Created Order table with mock data

--- src/app/sections/section-orders/section-orders.component.ts
+++ src/app/sections/section-orders/section-orders.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Order } from "../../shared/order";
 @Component({
   selector: 'app-section-orders',
   templateUrl: './section-orders.component.html',
@@ -8,6 +8,21 @@ import { Component, OnInit } from '@angular/core';
 export class SectionOrdersComponent implements OnInit {
 
   constructor() { }
+
+  orders: Order[] = [
+    {id: 1, 
+      customer:{id: 1,name:'customer1',
+      email:'customer1@gmai.com',provence:'ON'},
+      total: 2,placed: new Date(2022,5,14),fulfilled:new Date(2022,6,12)},
+    {id: 2, 
+      customer:{id: 2,name:'customer2',
+      email:'customer2@gmai.com',provence:'BC'},
+      total: 7,placed: new Date(2022,5,14),fulfilled:new Date(2022,6,12)},
+    {id: 3, 
+      customer:{id: 3,name:'customer3',
+      email:'customer3@gmai.com',provence:'MB'},
+      total: 5,placed: new Date(2022,5,14),fulfilled:new Date(2022,6,12)}
+  ];
 
   ngOnInit(): void {
   }
