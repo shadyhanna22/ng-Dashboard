1b0801ccb64ff45a2ad755cd595998f12d485b95
Shady Hanna <shady22@my.yorku.ca>
2022-07-17 20:35:48 -0400

    Updated server compponent

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,9 +10,29 @@ export class ServerComponent implements OnInit {
 
   constructor() { }
 
+  color: string;
+  buttonText: string;
+
   @Input() serverInput : Server;
 
   ngOnInit(): void {
+    this.setServerStatus(this.serverInput.isOnline);
   }
 
+  setServerStatus(isOnline: boolean){
+    if (isOnline) {
+      this.serverInput.isOnline = true;
+      this.color = '#66BB6A';
+      this.buttonText = 'Shut Down';
+    } else {
+      this.serverInput.isOnline = false;
+      this.color = '#ff6b6b';
+      this.buttonText = 'Start';
+    }
+  }
+
+  toggleStatus(OnlineStatus: boolean){
+     console.log(this.serverInput.name, ': ', OnlineStatus);
+     this.setServerStatus(!OnlineStatus);
+  }
 }
